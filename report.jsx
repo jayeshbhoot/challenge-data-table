835fe4e5604df8f3b74be4998e2b0903d547ad92
Jayesh Bhoot <jayesh@bhoot.sh>
2019-07-27 10:57:10 +0530

    - Add meaningful dimensions - OS and Browser - Improve handling of NaN values

--- report.jsx
+++ report.jsx
@@ -6,7 +6,9 @@ var rows = require('./data.json')
 
 var dimensions = [
   { value: 'date', title: 'Date' },
-  { value: 'host', title: 'Host' }
+  { value: 'host', title: 'Host' },
+  { value: 'uaOS', title: 'OS' },
+  { value: 'uaBrowser', title: 'Browser' }
 ]
 
 var reducer = function (row, acc) {
@@ -26,13 +28,29 @@ var reducer = function (row, acc) {
 }
 
 var calculations = [
-  { title: 'Impressions', value: 'impressions' },
-  { title: 'Loads', value: 'loads' },
-  { title: 'Displays', value: 'displays' },
+  {
+    title: 'Impressions', value: 'impressions',
+    template: function (val, row) {
+      return `${val || 0}`
+    }
+  },
+  {
+    title: 'Loads', value: 'loads',
+    template: function (val, row) {
+      return `${val || 0}`
+    }
+  },
+  {
+    title: 'Displays', value: 'displays',
+    template: function (val, row) {
+      return `${val || 0}`
+    }
+  },
   {
     title: 'Load Rate', value: 'loadRate',
     template: function (val, row) {
-      return (row.loads / row.impressions * 100).toFixed(1) + '%'
+      var loadRate = (row.loads / row.impressions) || 0
+      return `${(loadRate * 100).toFixed(1)}%`
     },
     sortBy: function (row) {
       return row.loads / row.impressions
@@ -41,7 +59,8 @@ var calculations = [
   {
     title: 'Display Rate', value: 'displayRate',
     template: function (val, row) {
-      return (row.displays / row.loads * 100).toFixed(1) + '%'
+      var displayRate = (row.displays / row.loads) || 0
+      return `${(displayRate * 100).toFixed(1)}%`
     },
     sortBy: function (row) {
       return row.displays / row.loads
