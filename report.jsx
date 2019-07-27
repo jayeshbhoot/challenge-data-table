3f97c640f839624f20db360b236f83d371946869
Jayesh Bhoot <jayesh@bhoot.sh>
2019-07-27 11:26:29 +0530

    - Improve adherence to coding standard: ordering - Add timezone as a dimension - Modularize template functions

--- report.jsx
+++ report.jsx
@@ -2,19 +2,34 @@ var React = require('react')
 var ReactPivot = require('react-pivot')
 var createReactClass = require('create-react-class')
 
-var rows = require('./data.json')
+module.exports = createReactClass({
+  render() {
+    return (<ReactPivot rows={rows}
+      dimensions={dimensions}
+      reduce={reducer}
+      calculations={calculations}
+      activeDimensions={['Date', 'Host']} />)
+  }
+})
 
-var dimensions = [
+const rows = require('./data.json')
+
+const dimensions = [
   { value: 'date', title: 'Date' },
   { value: 'host', title: 'Host' },
   { value: 'uaOS', title: 'OS' },
-  { value: 'uaBrowser', title: 'Browser' }
+  { value: 'uaBrowser', title: 'Browser' },
+  // tz could have been shown better (UTC+x)
+  // if I had found a way to templatize the value (UTC+x) like in calculations.
+  // in case I find a way:
+  // const hours = val / 60
+  // return hours >= 0 ? `UTC-${hours}` : `UTC+${Math.abs(hours)}`
+  { value: 'timezoneOffset', title: 'Timezone' }
 ]
 
 var reducer = function (row, acc) {
   // acc.impressions = row.type === 'impression' ? (acc.impressions || 0) : (acc.impressions || 0) + 1
   // acc.impressions = (acc.impressions || 0) + (row.type === 'impression' ? 1 : 0)
-
   if (row.type === 'impression')
     acc.impressions = (acc.impressions || 0) + 1
 
@@ -28,29 +43,13 @@ var reducer = function (row, acc) {
 }
 
 var calculations = [
-  {
-    title: 'Impressions', value: 'impressions',
-    template: function (val, row) {
-      return `${val || 0}`
-    }
-  },
-  {
-    title: 'Loads', value: 'loads',
-    template: function (val, row) {
-      return `${val || 0}`
-    }
-  },
-  {
-    title: 'Displays', value: 'displays',
-    template: function (val, row) {
-      return `${val || 0}`
-    }
-  },
+  { title: 'Impressions', value: 'impressions', template: templateForReducedVals },
+  { title: 'Loads', value: 'loads', template: templateForReducedVals },
+  { title: 'Displays', value: 'displays', template: templateForReducedVals },
   {
     title: 'Load Rate', value: 'loadRate',
     template: function (val, row) {
-      var loadRate = (row.loads / row.impressions) || 0
-      return `${(loadRate * 100).toFixed(1)}%`
+      return templateForPercentVals(row.loads, row.impressions)
     },
     sortBy: function (row) {
       return row.loads / row.impressions
@@ -59,8 +58,7 @@ var calculations = [
   {
     title: 'Display Rate', value: 'displayRate',
     template: function (val, row) {
-      var displayRate = (row.displays / row.loads) || 0
-      return `${(displayRate * 100).toFixed(1)}%`
+      return templateForPercentVals(row.displays, row.loads)
     },
     sortBy: function (row) {
       return row.displays / row.loads
@@ -68,12 +66,11 @@ var calculations = [
   }
 ]
 
-module.exports = createReactClass({
-  render() {
-    return (<ReactPivot rows={rows}
-      dimensions={dimensions}
-      reduce={reducer}
-      calculations={calculations}
-      activeDimensions={['Date', 'Host']} />)
-  }
-})
+const templateForReducedVals = function (val) {
+  return `${val || 0}`
+}
+
+const templateForPercentVals = function (numerator, denominator) {
+  const rate = (numerator / denominator) || 0
+  return `${(rate * 100).toFixed(1)}%`
+}
